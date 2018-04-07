dd85ab650556f21215d0860927ddf24dc57348ce
cyclonia <myworldsunil@gmail.com>
2018-04-07 10:57:32 +0530

    First Commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sunil on 07/04/2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello world  : " +i);
+        }
+    }
+}
